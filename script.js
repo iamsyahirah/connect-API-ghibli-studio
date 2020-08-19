e04106ddf19f5b2721b9c96e23fb036a32bac0ec
syahirah <iamsyahirah@users.noreply.github.com>
2020-08-19 16:23:56 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+request.onload = function() {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach((movie) => {
+            // Create a div with a card class
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            // Create an h1 and set the text content to the film's title
+            const h1 = document.createElement('h1')
+            h1.textContent = movie.title
+
+
+            const h2 = document.createElement('h2')
+            h2.textContent = movie.release_date
+
+            // Create a p and set the text content to the film's description
+            const p = document.createElement('p')
+            movie.description = movie.description.substring(0, 200) // Limit to 300 chars
+            p.textContent = `${movie.description}...` // End with an ellipses
+
+            // Append the cards to the container element
+            container.appendChild(card)
+
+            // Each card will contain and h1 and a p
+            card.appendChild(h1)
+            card.appendChild(h2)
+            card.appendChild(p)
+        })
+    } else {
+        const errorMessage = document.createElement('marquee')
+        errorMessage.textContent = `NO, it's not working`
+        app.appendChild(errorMessage)
+    }
+
+}
+
+// Send request
+request.send()
